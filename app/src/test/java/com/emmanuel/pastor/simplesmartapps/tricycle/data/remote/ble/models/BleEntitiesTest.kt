66e93ebb53b7d7e9ee43376f6b6f21a7f3178097
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-12-07 18:13:03 +0100

    Update BLE entities tests
    Signed-off-by: Emmanuel Pastor <ommanu13@hotmail.fr>

--- app/src/test/java/com/emmanuel/pastor/simplesmartapps/tricycle/data/remote/ble/models/BleEntitiesTest.kt
+++ app/src/test/java/com/emmanuel/pastor/simplesmartapps/tricycle/data/remote/ble/models/BleEntitiesTest.kt
@@ -4,12 +4,26 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 import org.junit.Test
 
+@OptIn(ExperimentalUnsignedTypes::class)
 class BleEntitiesTest {
-    @OptIn(ExperimentalUnsignedTypes::class)
     @Test
     fun `create BatteryBleEntity from unsigned byte array`() = runBlocking {
         val uByteArray = ubyteArrayOf(0x50u)
-        val batteryBleEntity = BatteryBleEntity.fromUByteArray(uByteArray)
-        assertEquals(80, batteryBleEntity.percentage)
+        val batteryBleEntity = BatteryBleEntity.fromUByteArrayOrNull(uByteArray)
+        assertEquals(80, batteryBleEntity?.percentage)
+    }
+
+    @Test
+    fun `create LoadBleEntity from unsigned byte array`() = runBlocking {
+        val uByteArray = ubyteArrayOf(0xFFu, 0x01u)
+        val loadBleEntity = LoadBleEntity.fromUByteArrayOrNull(uByteArray)
+        assertEquals(511, loadBleEntity?.loadWeight)
+    }
+
+    @Test
+    fun `create MileageBleEntity from unsigned byte array`() = runBlocking {
+        val uByteArray = ubyteArrayOf(0x80u, 0xCCu, 0x06u, 0x02u)
+        val mileageBleEntity = MileageBleEntity.fromUByteArrayOrNull(uByteArray)
+        assertEquals(34_000_000, mileageBleEntity?.mileage)
     }
 }
