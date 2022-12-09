ec3cbc5a2fa897f792b91ea118afdad31a6fe859
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-12-09 11:35:18 +0100

    Setup theming of UI
    Signed-off-by: Emmanuel Pastor <ommanu13@hotmail.fr>

--- app/src/main/java/com/emmanuel/pastor/simplesmartapps/tricycle/ui/theme/Theme.kt
+++ app/src/main/java/com/emmanuel/pastor/simplesmartapps/tricycle/ui/theme/Theme.kt
@@ -6,38 +6,50 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = FadedBlue,
+    onPrimary = ElectricBlue,
+    secondary = FadedBlueA15,
+    tertiary = FadedOrange,
+    surface = DarkGrey,
+    onSurface = Color.White,
+    background = AlmostBlack,
+    onBackground = Color.White,
+    error = FadedErrorRed,
 )
+
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = MainBlue,
+    secondary = MainBlueA15,
+    tertiary = MainOrange,
+    surface = Color.White,
+    onSurface = Color.Black,
+    background = BlueWhite,
+    onBackground = Color.Black,
+    error = ErrorRed,
 )
+
+val ColorScheme.success: Color
+    @Composable get() = if (isSystemInDarkTheme()) FadedGreen500 else Green500
+val ColorScheme.warning: Color
+    @Composable get() = if (isSystemInDarkTheme()) FadedOrange else MainOrange
+val ColorScheme.onSurfaceOff: Color
+    @Composable get() = if (isSystemInDarkTheme()) FadedOffGrey else OffGrey
+val ColorScheme.onSurfaceDiscrete: Color
+    @Composable get() = Grey500
 
 @Composable
 fun TricycleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
