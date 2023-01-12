f95a270c08637f360bbc7f82bef124acef548903
Emmanuel Pastor <ommanu13@hotmail.fr>
2023-01-12 10:13:52 +0100

    Handle permissions in MainActivity
    Signed-off-by: Emmanuel Pastor <ommanu13@hotmail.fr>

--- app/src/main/java/com/emmanuel/pastor/simplesmartapps/tricycle/MainActivity.kt
+++ app/src/main/java/com/emmanuel/pastor/simplesmartapps/tricycle/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.emmanuel.pastor.simplesmartapps.tricycle
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
@@ -13,21 +19,32 @@ import com.emmanuel.pastor.simplesmartapps.tricycle.ui.nav.BottomNavigation
 import com.emmanuel.pastor.simplesmartapps.tricycle.ui.nav.NavigationGraph
 import com.emmanuel.pastor.simplesmartapps.tricycle.ui.nav.TopNavigation
 import com.emmanuel.pastor.simplesmartapps.tricycle.ui.theme.TricycleTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TricycleTheme {
                 val navController = rememberNavController()
                 val viewModel: MainViewModel = hiltViewModel()
+                val tricycleServicePermissions = rememberMultiplePermissionsState(getTricycleServicePermissions())
                 val connectionState by viewModel.connectionState.collectAsState()
 
                 Scaffold(
-                    topBar = { TopNavigation(connectionState = connectionState, viewModel::onReconnectClicked) },
+                    topBar = {
+                        TopNavigation(connectionState = connectionState) {
+                            if (tricycleServicePermissions.allPermissionsGranted) {
+                                viewModel.onReconnectClicked()
+                            } else {
+                                tricycleServicePermissions.launchMultiplePermissionRequest()
+                            }
+                        }
+                    },
                     bottomBar = { BottomNavigation(navController = navController) },
                     content = { innerPadding ->
                         Surface(color = MaterialTheme.colorScheme.background, modifier = Modifier.padding(innerPadding)) {
@@ -38,4 +55,21 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun getTricycleServicePermissions() = mutableListOf<String>().apply {
+        // Bluetooth permissions
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            add(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+            add(Manifest.permission.BLUETOOTH_ADMIN)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            add(Manifest.permission.BLUETOOTH_CONNECT)
+            add(Manifest.permission.BLUETOOTH_SCAN)
+        }
+
+        // Notification permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            add(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }.toList()
 }
--- app/src/main/java/com/emmanuel/pastor/simplesmartapps/tricycle/MainViewModel.kt
+++ app/src/main/java/com/emmanuel/pastor/simplesmartapps/tricycle/MainViewModel.kt
@@ -1,16 +1,25 @@
 package com.emmanuel.pastor.simplesmartapps.tricycle
 
+import android.app.Application
+import android.content.Context
+import android.content.Intent
 import android.util.Log
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.emmanuel.pastor.simplesmartapps.tricycle.platform.BleClient
+import com.emmanuel.pastor.simplesmartapps.tricycle.services.TricycleService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val bleClient: BleClient) : ViewModel() {
+class MainViewModel @Inject constructor(private val bleClient: BleClient, application: Application) : AndroidViewModel(application) {
+    companion object {
+        private const val TAG = "MainViewModel"
+        private const val TRICYCLE_SERIAL_NUMBER = "7836082"
+    }
+
     private val _isLoading = MutableStateFlow(false)
     val connectionState: StateFlow<ConnectionState> = bleClient.isConnectedStateFlow.combine(_isLoading) { isConnected, isLoading ->
         ConnectionState(isConnected, isLoading)
@@ -20,11 +29,20 @@ class MainViewModel @Inject constructor(private val bleClient: BleClient) : View
         initialValue = ConnectionState(isConnected = false, isLoading = false)
     )
 
+    /**
+     * Requires Bluetooth permissions
+     *
+     * Requires Notification permission on [android.os.Build.VERSION_CODES.TIRAMISU] and above
+     */
     fun onReconnectClicked() {
         viewModelScope.launch {
             _isLoading.value = true
-            bleClient.openConnection("AAAAAAAAAA").onFailure {
-                Log.e("MainViewModel", "${it.message}")
+            bleClient.openConnection(TRICYCLE_SERIAL_NUMBER).onSuccess {
+                with(getApplication() as Context) {
+                    startForegroundService(Intent(this, TricycleService::class.java))
+                }
+            }.onFailure {
+                Log.e(TAG, "${it.message}")
             }
         }.invokeOnCompletion { _isLoading.value = false }
     }
